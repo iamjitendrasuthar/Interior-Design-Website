072e446e8d672a0907378b9e6c8c14974bc55dca
jitendra <iamjitendrasuthar24@gmail.com>
2026-03-16 17:02:02 +0530

    Added tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,18 @@ import WhatsAppWidget from "@/components/WhatsAppWidget";
 export const metadata = {
   title: "Js Interiors",
   description: "Premium Interior Design for Modern Homes",
+  icons: {
+    icon: "https://framerusercontent.com/images/fNCAHGamGdHb8NnhNH6TCD49o.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white text-[#132A13] font-sans selection:bg-[#132A13] selection:text-white" style={{position:"relative"}}>
+      <body
+        className="bg-white text-[#132A13] font-sans selection:bg-[#132A13] selection:text-white"
+        style={{ position: "relative" }}
+      >
         <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />
